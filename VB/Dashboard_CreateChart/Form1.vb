0e853571b8f07ae1e977362865f070a539714d80
DevExpressExampleBot <fakeEmail>
2023-10-06 13:02:29 +0400

    Source auto update [skip ci]

--- VB/Dashboard_CreateChart/Form1.vb
+++ VB/Dashboard_CreateChart/Form1.vb
@@ -1,66 +1,58 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.DashboardCommon
 Imports DevExpress.DataAccess
 
 Namespace Dashboard_CreateChart
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Function CreateChart(ByVal dataSource As DataSource) As ChartDashboardItem
 
-			' Creates a chart dashboard item and specifies its data source.
-			Dim chart As New ChartDashboardItem()
-			chart.DataSource = dataSource
+    Public Partial Class Form1
+        Inherits Form
 
-			' Specifies the dimension used to provide data for arguments on a chart.
-			chart.Arguments.Add(New Dimension("Sales Person", DateTimeGroupInterval.Year))
-
-			' Specifies the dimension that provides data for chart series.
-			chart.SeriesDimensions.Add(New Dimension("OrderDate"))
-
-			' Adds a new chart pane to the chart's Panes collection.
-			chart.Panes.Add(New ChartPane())
-			' Creates a new series of the Bar type.
-			Dim salesAmountSeries As New SimpleSeries(SimpleSeriesType.Bar)
-			' Specifies the measure that provides data used to calculate
-			' the Y-coordinate of data points.
-			salesAmountSeries.Value = New Measure("Extended Price")
-			' Adds created series to the pane's Series collection to display within this pane.
-			chart.Panes(0).Series.Add(salesAmountSeries)
-
-			chart.Panes.Add(New ChartPane())
-			Dim taxesAmountSeries As New SimpleSeries(SimpleSeriesType.StackedBar)
-			taxesAmountSeries.Value = New Measure("Quantity")
-			chart.Panes(1).Series.Add(taxesAmountSeries)
-
-			Return chart
-		End Function
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-
-			' Creates a dashboard and sets it as the currently opened dashboard in the dashboard viewer.
-			dashboardViewer1.Dashboard = New Dashboard()
-
-			' Creates a data source and adds it to the dashboard data source collection.
-			Dim dataSource As New DataSource("Sales Person")
-			dashboardViewer1.Dashboard.DataSources.Add(dataSource)
-
-			' Creates a chart dashboard item with the specified data source 
-			' and adds it to the Items collection to display within the dashboard.
-			Dim chart As ChartDashboardItem = CreateChart(dataSource)
-			dashboardViewer1.Dashboard.Items.Add(chart)
-
-			' Reloads data in the data sources.
-			dashboardViewer1.ReloadData()
-		End Sub
-        Private Sub dashboardViewer1_DataLoading(ByVal sender As Object, _
-                    ByVal e As DataLoadingEventArgs) Handles dashboardViewer1.DataLoading
-
-            ' Specifies data for the current data source.
-            e.Data = (New nwindDataSetTableAdapters.SalesPersonTableAdapter()).GetData()
+        Public Sub New()
+            InitializeComponent()
         End Sub
-	End Class
+
+        Private Function CreateChart(ByVal dataSource As DataSource) As ChartDashboardItem
+            ' Creates a chart dashboard item and specifies its data source.
+            Dim chart As ChartDashboardItem = New ChartDashboardItem()
+            chart.DataSource = dataSource
+            ' Specifies the dimension used to provide data for arguments on a chart.
+            chart.Arguments.Add(New Dimension("Sales Person", DateTimeGroupInterval.Year))
+            ' Specifies the dimension that provides data for chart series.
+            chart.SeriesDimensions.Add(New Dimension("OrderDate"))
+            ' Adds a new chart pane to the chart's Panes collection.
+            chart.Panes.Add(New ChartPane())
+            ' Creates a new series of the Bar type.
+            Dim salesAmountSeries As SimpleSeries = New SimpleSeries(SimpleSeriesType.Bar)
+            ' Specifies the measure that provides data used to calculate
+            ' the Y-coordinate of data points.
+            salesAmountSeries.Value = New Measure("Extended Price")
+            ' Adds created series to the pane's Series collection to display within this pane.
+            chart.Panes(0).Series.Add(salesAmountSeries)
+            chart.Panes.Add(New ChartPane())
+            Dim taxesAmountSeries As SimpleSeries = New SimpleSeries(SimpleSeriesType.StackedBar)
+            taxesAmountSeries.Value = New Measure("Quantity")
+            chart.Panes(1).Series.Add(taxesAmountSeries)
+            Return chart
+        End Function
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' Creates a dashboard and sets it as the currently opened dashboard in the dashboard viewer.
+            dashboardViewer1.Dashboard = New Dashboard()
+            ' Creates a data source and adds it to the dashboard data source collection.
+            Dim dataSource As DataSource = New DataSource("Sales Person")
+            dashboardViewer1.Dashboard.DataSources.Add(dataSource)
+            ' Creates a chart dashboard item with the specified data source 
+            ' and adds it to the Items collection to display within the dashboard.
+            Dim chart As ChartDashboardItem = CreateChart(dataSource)
+            dashboardViewer1.Dashboard.Items.Add(chart)
+            ' Reloads data in the data sources.
+            dashboardViewer1.ReloadData()
+        End Sub
+
+        Private Sub dashboardViewer1_DataLoading(ByVal sender As Object, ByVal e As DataLoadingEventArgs)
+            ' Specifies data for the current data source.
+            e.Data =(New nwindDataSetTableAdapters.SalesPersonTableAdapter()).GetData()
+        End Sub
+    End Class
 End Namespace
